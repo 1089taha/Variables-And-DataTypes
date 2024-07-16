3af372e6cae1a821c4fed556cad79f1755f17dfa
1089taha <161455916+1089taha@users.noreply.github.com>
2024-07-16 16:29:32 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+//STRING:
+var MyName = "Taha Khan";
+console.log(MyName);
+//NUMBER:
+var fristinningsscore = 120;
+console.log(fristinningsscore);
+{
+    var fristinningsscore_1 = 220;
+    console.log(fristinningsscore_1);
+}
+console.log(fristinningsscore);
+//BOOLEAN:
+var thursdaystudent = true;
+console.log(thursdaystudent);
+//ANY:
+var MyFullName = "TAHA";
+console.log(MyFullName);
+var secondinningscore = 181;
+console.log(secondinningscore);
+var tuesdaystudent = true;
+console.log(tuesdaystudent);
+// UNKMOWN
+var userName = "Taha";
+console.log(userName);
+// UNDEFINED
+var noValue = undefined;
+var no_value;
+// NULL 
+var myValue = null;
+console.log(myValue);
---
+++ index.ts
@@ -0,0 +1,38 @@
+//STRING:
+ let MyName: string = "Taha Khan"
+ console.log(MyName);
+
+ //NUMBER:
+ const fristinningsscore: number = 120
+ console.log(fristinningsscore)
+ {
+     const fristinningsscore:number= 220
+     console.log(fristinningsscore)
+ }
+ console.log(fristinningsscore)
+
+ //BOOLEAN:
+ let thursdaystudent: boolean = true
+ console.log(thursdaystudent);
+
+ //ANY:
+let MyFullName: any = "TAHA"
+  console.log(MyFullName)
+
+  let secondinningscore: any = 181
+  console.log(secondinningscore)
+
+  let tuesdaystudent: any= true
+  console.log(tuesdaystudent)
+
+ // UNKMOWN
+ let userName: unknown = "Taha";
+  console.log(userName);
+
+ // UNDEFINED
+  let noValue = undefined;
+  let no_value;
+
+ // NULL 
+ let myValue:null=null;
+ console.log(myValue);
